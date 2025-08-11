2101bb02b5fcd4e3f31b62c24d8ebc25fe7208fb
nishiiko <nishiiko@protonmail.com>
2025-08-11 21:48:40 +0200

    modified:   autostart.sh

--- .config/bspwm/bspwm.d/autostart.sh
+++ .config/bspwm/bspwm.d/autostart.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 picom &
-nitrogen --set-zoom-fill $HOME/Pictures/Wallpapers/otherworld_dark.png &
-polybar &
+unclutter --timeout 3 --start-hidden -b &
 xsetroot -cursor_name left_ptr &
 fcitx5 -d &
 setxkbmap -layout "gb(colemak_dh),gb" -option "grp:win_space_toggle" &
+polybar & 
+nitrogen --set-zoom-fill $HOME/Pictures/Wallpapers/otherworld_dark.png &
 dunst --startup_notification &
 $HOME/.config/bspwm/bspwm.d/pipewire.sh &
